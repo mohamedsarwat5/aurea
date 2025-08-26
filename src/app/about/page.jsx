0d9055c8b4aaf24d2f901dddd512ba939a13f105
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-08-27 00:10:10 +0300

    remove h

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <div className='bg-light'>
       <div className=' c flex flex-col gap-y-3  '>
         <Header  title='about us' desc="The Heart Of Auréa"></Header>
-        <section className='flex-1 flex flex-col md:flex-row md:justify-between justify-center min-h-[400px] items-center gap-y-9 '>
+        <section className='flex-1 flex flex-col md:flex-row md:justify-between justify-center  items-center gap-y-9 '>
           <div className='order-1 flex flex-col gap-y-2'>
               <h2 className=' md:text-4xl text-[22px] text-dark-green '>Clear Skin, clear confidence</h2>
               <p className='md:w-[600px] leading-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad enim dolorum aperiam id minima veniam veritatis voluptatem. Accusamus quasi possimus rerum! Aliquid illo molestias quasi voluptate, animi nostrum dignissimos quod!</p>
