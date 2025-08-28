89b7267e9e2a30ab9e04a3371f55b4539e81bf09
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-08-28 23:02:39 +0300

    be

--- src/app/beData.js
+++ src/app/beData.js
@@ -1,22 +1,22 @@
 const beData = [
     {
         "title": "Deep Hydration",
-        "desc": "Socks keep skin soft, smooth",
+        "desc": "Socks keep skin soft, smooth.",
         "icon": <img className="" src="/i1.png" alt="" />
     },
     {
         "title": "Radiance Boost",
-        "desc": "Ereichand adth naturontin",
+        "desc": "Ereichand adth naturontin.",
         "icon": <img className="" src="/i2.png" alt="" />
     },
     {
         "title": "Skin Barrier Support",
-        "desc": "Tomtomaty & peanut extract nourish, protect",
+        "desc": "Shields and strengthens skin.",
         "icon": <img className="" src="/i3.png" alt="" />
     },
     {
         "title": "Powered by Natural Ingredients",
-        "desc": "Tomtomaty & peanut extract nourish, protect",
+        "desc": "Natural plant oils for nourishing, chemical-free care.",
         "icon": <img className="" src="/i4.png" alt="" />
     }
 ]
