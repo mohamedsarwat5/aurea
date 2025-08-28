6ec12a90a116b09c5ffc0443b0b1c9f325cd5a64
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-08-28 23:04:56 +0300

    fa

--- src/app/ToTop.jsx
+++ src/app/ToTop.jsx
@@ -3,7 +3,7 @@
 export default function ToTop() {
     return (
         <>
-           <div onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='flex items-center justify-center w-10 h-10 bg-green cursor-pointer fixed lg:end-5 lg:bottom-5 end-3 bottom-3 rounded-full text-white'>
+           <div onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='flex items-center justify-center w-10 z-[84498423] h-10 bg-green cursor-pointer fixed lg:end-5 lg:bottom-5 end-3 bottom-3 rounded-full text-white'>
                 <i className="fa-solid fa-arrow-up"></i>
             </div>
 
