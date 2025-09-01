47c5323c6e325b1ec7cee8836ecf9a0625f31c5e
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-09-01 22:36:04 +0300

    fa

--- src/app/FormSubscribe/FormSubscribe.jsx
+++ src/app/FormSubscribe/FormSubscribe.jsx
@@ -4,14 +4,19 @@ import { useFormik } from 'formik'
 import React, { useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { toast } from 'react-toastify'
+import * as Yup from 'yup';
+
 export default function FormSubscribe() {
 
     const [loading, setLoading] = useState(false)
-
+    const validationSchema = Yup.object({
+        email: Yup.string().matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Invalid email format').required('Please Enter your Email')
+    })
     const formik = useFormik({
         initialValues: {
             email: ''
         },
+        validationSchema,
         onSubmit: (values, { resetForm }) => {
             setLoading(true)
             emailjs.send(
@@ -35,9 +40,12 @@ export default function FormSubscribe() {
     return (<>
         <form onSubmit={formik.handleSubmit} className="max-w-md md:mt-8 mt-4">
             <div className="relative">
-                <input onChange={formik.handleChange} value={formik.values.email} name='email' type="email" id="default-search" className="block w-full p-4 pe-[150px] overflow-hidden text-base text-dark-green  rounded-lg bg-white-50 focus:ring-green outline-0 shadow-lg " placeholder="Enter your email here" required />
+                <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} name='email' type="email" id="default-search" className="block w-full p-4 pe-[150px] overflow-hidden text-base text-dark-green  rounded-lg bg-white-50 focus:ring-green outline-0 shadow-lg " placeholder="Enter your email here" required />
                 <button disabled={loading} type="submit" className={`  flex items-center gap-2 text-white absolute end-2.5 bottom-2.5 ${loading ? "bg-green/30 cursor-not-allowed" : "bg-green hover:bg-green/80 cursor-pointer"} transition-all duration-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 `}> {loading && <span className="loader"></span>} Subscribe </button>
+
             </div>
+            {formik.touched.email && formik.errors.email && <p className='text-red-700 text-base mt-1 ms-2
+                '>{formik.errors.email}</p>}
         </form>
     </>)
 }
